a461eaa7119644514375d3c4cbce2b3ec931972f
Rafael Ibraim <ibraim.gm@gmail.com>
2019-05-31 22:04:33 -0300

    Implementação do filtro (filter)

--- src/molecules/Filter.jsx
+++ src/molecules/Filter.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+import Button from '../atoms/Button';
 
 const Filter = props => {
   const { items } = props;
 
   return (
-    <div>
-      Filter:
-      {items
-        .map(it => (typeof it === 'string' ? { name: it, active: false } : it))
-        .map((it, i) => (
-          <b key={i}>{it.name}&nbsp;</b>
-        ))}
-    </div>
+    <nav className="filter">
+      <ol className="list-unstyled components filter__list">
+        {items
+          .map(it => (typeof it === 'string' ? { name: it } : it))
+          .map((it, i) => {
+            const { active } = it;
+            const css = classNames('filter__button', {
+              'filter__button--active': active
+            });
+
+            return (
+              <li key={i} className="filter__item">
+                <Button className={css} caption={it.name} />
+              </li>
+            );
+          })}
+      </ol>
+    </nav>
   );
 };
 
