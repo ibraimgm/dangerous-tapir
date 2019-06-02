38c77df098ce2534536242b3888c0228b3fe1f0b
Rafael Ibraim <ibraim.gm@gmail.com>
2019-06-02 12:03:00 -0300

    Updates

---
+++ precache-manifest.26ae58156c6d0457d8664df9b1c6430d.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef20d7fef1bcca26cfd2935c68d4fe43",
+    "url": "/dangerous-tapir/index.html"
+  },
+  {
+    "revision": "e019491ccac755f8ce2e",
+    "url": "/dangerous-tapir/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "7e0675a6f668b79f024e",
+    "url": "/dangerous-tapir/static/css/main.979d6ed2.chunk.css"
+  },
+  {
+    "revision": "e019491ccac755f8ce2e",
+    "url": "/dangerous-tapir/static/js/2.2972c9bc.chunk.js"
+  },
+  {
+    "revision": "7e0675a6f668b79f024e",
+    "url": "/dangerous-tapir/static/js/main.997c308e.chunk.js"
+  },
+  {
+    "revision": "adf51dbcc5e929262cfc",
+    "url": "/dangerous-tapir/static/js/runtime~main.fff5583b.js"
+  },
+  {
+    "revision": "0ce188a10d31206039f52feead1e08b8",
+    "url": "/dangerous-tapir/static/media/Avenir-Black.0ce188a1.svg"
+  },
+  {
+    "revision": "12f59411790b7965b4000692082b764d",
+    "url": "/dangerous-tapir/static/media/Avenir-Black.12f59411.ttf"
+  },
+  {
+    "revision": "4b8435697db4b2075bdf4511b4953f78",
+    "url": "/dangerous-tapir/static/media/Avenir-Black.4b843569.eot"
+  },
+  {
+    "revision": "574f812873279099a0bca1cc1384233f",
+    "url": "/dangerous-tapir/static/media/Avenir-Black.574f8128.woff2"
+  },
+  {
+    "revision": "9aef078fc98c67ed18dac59af49b038c",
+    "url": "/dangerous-tapir/static/media/Avenir-Black.9aef078f.woff"
+  },
+  {
+    "revision": "065098a14396b08f2177ee0a57f2a02a",
+    "url": "/dangerous-tapir/static/media/Avenir-Heavy.065098a1.eot"
+  },
+  {
+    "revision": "13fc111e74e108819ffbb7ef10bebca7",
+    "url": "/dangerous-tapir/static/media/Avenir-Heavy.13fc111e.svg"
+  },
+  {
+    "revision": "456945210abb06974bcf0572c221875d",
+    "url": "/dangerous-tapir/static/media/Avenir-Heavy.45694521.ttf"
+  },
+  {
+    "revision": "79efafde076faa22837124f229bee439",
+    "url": "/dangerous-tapir/static/media/Avenir-Heavy.79efafde.woff"
+  },
+  {
+    "revision": "bd71ce51adfd1cec1fb328d581503511",
+    "url": "/dangerous-tapir/static/media/Avenir-Heavy.bd71ce51.woff2"
+  },
+  {
+    "revision": "3045bf99caf4d0c23213886ebc0657eb",
+    "url": "/dangerous-tapir/static/media/Avenir-Medium.3045bf99.woff2"
+  },
+  {
+    "revision": "3bd6428281856b1582bfb49d64247c99",
+    "url": "/dangerous-tapir/static/media/Avenir-Medium.3bd64282.eot"
+  },
+  {
+    "revision": "922ec9af3e7235d366eca8a8511af253",
+    "url": "/dangerous-tapir/static/media/Avenir-Medium.922ec9af.ttf"
+  },
+  {
+    "revision": "a979c8f0652f528dad9c872c52dd82cb",
+    "url": "/dangerous-tapir/static/media/Avenir-Medium.a979c8f0.woff"
+  },
+  {
+    "revision": "db6b04e952b61c78fabc94bc4a2f62e1",
+    "url": "/dangerous-tapir/static/media/Avenir-Medium.db6b04e9.svg"
+  },
+  {
+    "revision": "3a23c970a022643d9e7ffb1adec03672",
+    "url": "/dangerous-tapir/static/media/Avenir-Roman.3a23c970.woff2"
+  },
+  {
+    "revision": "3d77d56a0fa5e64b7917f8fd309f8f14",
+    "url": "/dangerous-tapir/static/media/Avenir-Roman.3d77d56a.ttf"
+  },
+  {
+    "revision": "4913a3449abeacde129173fd7ee74e7b",
+    "url": "/dangerous-tapir/static/media/Avenir-Roman.4913a344.svg"
+  },
+  {
+    "revision": "8b57827798d47c206c11a3a306493ff6",
+    "url": "/dangerous-tapir/static/media/Avenir-Roman.8b578277.eot"
+  },
+  {
+    "revision": "969df74ccb5d459886c5f2e01ca5a40b",
+    "url": "/dangerous-tapir/static/media/Avenir-Roman.969df74c.woff"
+  },
+  {
+    "revision": "ab6973fd7019edc8f9f55fe05de7d99a",
+    "url": "/dangerous-tapir/static/media/DIN-Condensed-Bold.ab6973fd.eot"
+  },
+  {
+    "revision": "e549d2e6c11fcac1ec8f739a778f7b44",
+    "url": "/dangerous-tapir/static/media/DIN-Condensed-Bold.e549d2e6.woff2"
+  },
+  {
+    "revision": "0a62ed09386c17c2654efc47e8f8355e",
+    "url": "/dangerous-tapir/static/media/DINCondensed-Bold.0a62ed09.svg"
+  },
+  {
+    "revision": "e1b1bb8fb482e6fad6997353a3bcadec",
+    "url": "/dangerous-tapir/static/media/DINCondensed-Bold.e1b1bb8f.ttf"
+  },
+  {
+    "revision": "e9295de7d1510e026077e9257690777a",
+    "url": "/dangerous-tapir/static/media/DINCondensed-Bold.e9295de7.woff"
+  },
+  {
+    "revision": "db3349ccdd84dba2ec5dba40dada660c",
+    "url": "/dangerous-tapir/static/media/ic_cadastro.db3349cc.svg"
+  },
+  {
+    "revision": "514fbbc273fe57abb9157a2a7b2c6d91",
+    "url": "/dangerous-tapir/static/media/ic_clientes.514fbbc2.svg"
+  },
+  {
+    "revision": "ff09e399fd5e3fda86e8662099f26b21",
+    "url": "/dangerous-tapir/static/media/ic_dashboard.ff09e399.svg"
+  },
+  {
+    "revision": "00946399a572e3346503fd1d1772e7d6",
+    "url": "/dangerous-tapir/static/media/ic_logo.00946399.svg"
+  },
+  {
+    "revision": "b77506d26debc91c5535248c1c76bc69",
+    "url": "/dangerous-tapir/static/media/ic_menu.b77506d2.svg"
+  },
+  {
+    "revision": "82ced422e1528242798273605b674c32",
+    "url": "/dangerous-tapir/static/media/ic_totais.82ced422.svg"
+  }
+]);
